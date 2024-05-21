b98ae8a8fd8e17d9fe6b0a4a249942f98b1cb2d4
Jean Carlo Nascimento <jnascimento@gmail.com>
2024-05-21 17:43:02 -0300

    listando os cursos de uma api

--- apps/course-list/src/components/CourseList.js
+++ apps/course-list/src/components/CourseList.js
@@ -1,14 +1,30 @@
-import React from 'react';
-import { useSelector } from 'react-redux';
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { setCourses, filterCourses } from '../../store';
+import axios from 'axios';
 
 const CourseList = () => {
+  const dispatch = useDispatch();
   const courses = useSelector(state => state.courses.list);
+
+  useEffect(() => {
+    const fetchCourses = async () => {
+      try {
+        const response = await axios.get('http://localhost:5000/courses');
+        dispatch(setCourses(response.data));
+      } catch (error) {
+        console.error('Erro ao buscar cursos:', error);
+      }
+    };
+
+    fetchCourses();
+  }, [dispatch]);
 
   return (
     <div>
       <h1>Lista de Cursos</h1>
       <ul>
-        {courses.map(course => (
+        {courses?.map(course => (
           <li key={course.id}>{course.name}</li>
         ))}
       </ul>
